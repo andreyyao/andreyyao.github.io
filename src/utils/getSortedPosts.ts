660bac623e89128269ff45dd255b4e88bed18470
Andrey Yao <awy32@cornell.edu>
2024-11-27 21:42:11 -0600

    Updated tags and blog posts structure based on upstream changes

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -3,6 +3,7 @@ import postFilter from "./postFilter";
 
 const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
   return posts
+    .filter(postFilter)
     .sort(
       (a, b) =>
         Math.floor(
