a9fe7c1599ce652de39406c54f6afc54a37c6df9
Andrey Yao <andrey.yao@wisc.edu>
2026-02-01 23:46:24 -0600

    Got rid of sharelinks and editposts

--- src/config.ts
+++ src/config.ts
@@ -10,11 +10,6 @@ export const SITE = {
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showBackButton: true, // show back button in post detail
-  editPost: {
-    enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
-  },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
